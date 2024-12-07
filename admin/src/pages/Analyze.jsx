e9c648d1787a938e6c84a28bf0c7e9d8d45305f1
nameissakthi <svel7252@gmail.com>
2024-12-07 16:01:04 +0530

    analyze print sections added

--- admin/src/pages/Analyze.jsx
+++ admin/src/pages/Analyze.jsx
@@ -2,8 +2,7 @@ import React from 'react'
 import axios from "axios";
 import { backendUrl } from "../App";
 import { toast } from "react-toastify";
-import { useState } from 'react';
-import { useEffect } from 'react';
+import { useState, useEffect, useRef } from 'react';
 
 const Analyze = ({currency}) => {
 
@@ -22,7 +21,6 @@ const Analyze = ({currency}) => {
             toast.error(error.message);
         }
     }
-
     
     const analysis = () => {
         let analyse = [0,0,0,0];
@@ -40,6 +38,48 @@ const Analyze = ({currency}) => {
 
     let analyse = analysis()
 
+    const handlePrint = () => {
+        const printWindow = window.open("", "", "width=800,height=600");
+        
+        printWindow.document.write(`
+            <html>
+              <head>
+                <title>Bill</title>
+                <style>
+                  body { font-family: Arial, sans-serif; }
+                  section { display : grid; grid-template-columns: repeat(2, minmax(0, 1fr)); gap : 10px; }
+                  div { border : 2px solid black; padding : 0.5rem 1rem; font-size: 1.25rem; line-height: 1.75rem; }
+                  h3 { margin-bottom : 0.5rem; font-size : 1.4rem; }
+                </style>
+              </head>
+              <body>
+                <h1>Analysis</h1>
+                <section>
+                    <div>
+                        <h3><b>Total Products Sold</b></h3>
+                        <span>${analyse[0]}</span>
+                    </div>
+                    <div>
+                        <h3><b>Total Cost Price</b></h3>
+                        <span>${currency}${analyse[1]}</span>
+                    </div>
+                    <div>
+                        <h3><b>Total Amount</b></h3>
+                        <span>${currency}${analyse[2]}</span>
+                    </div>
+                    <div>
+                        <h3><b>${analyse[3]>0?"Profit ↑":"Loss ↓"}</b></h3>
+                        <span>${currency}${analyse[3]}</span>
+                    </div>
+                </section>
+              </body>
+            </html>
+          `);
+
+        printWindow.document.close();
+        printWindow.print();
+    }
+
     useEffect(()=>{
         fetchHistory()
     },[])
@@ -49,7 +89,10 @@ const Analyze = ({currency}) => {
         <div>
             <div className='flex justify-between mb-3'>
                 <h1 className='text-xl'>Analysis</h1>
-                <button onClick={()=>window.location.reload()} className="text-base p-2 bg-violet-700 text-white rounded float-end">Refresh</button>
+                <div className='flex gap-2'>
+                    <button onClick={()=>window.location.reload()} className="text-base p-2 bg-green-700 text-white rounded float-end">Refresh</button>
+                    <button onClick={handlePrint} className="text-base p-2 bg-violet-700 text-white rounded float-end">Print</button>
+                </div>
             </div>
             <div className='grid grid-cols-2 mb-5 gap-2'>
                 <div className='text-xl border-2 p-4 py-8'>
